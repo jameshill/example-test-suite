435e064e2bd50095f137df505df59ce060bd5fb6
James Hill <395+jameshill@users.noreply.github.com.>
2024-11-29 01:16:49 +1100

    More tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,7 +1,63 @@
 require "spec_helper"
 
 describe "Cart", type: :model do
-  it "is a placeholder test" do
+  it "can add items to it" do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "can remove items from cart" do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "updates the quantity of a product in the cart successfully" do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "empties the cart successfully" do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "calculates the total price of the cart accurately" do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "validates cart contents before checkout" do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "processes the cart checkout successfully  " do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "ensures cart integrates with the inventory system " do
+    base_sleep_duration = rand(1..3)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+
+  it "handles errors during cart operations gracefully" do
     base_sleep_duration = rand(1..3)
     final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
     sleep final_sleep_duration.round(3)
