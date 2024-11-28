c87dc2f5f1aab882390864502b0cd21783493e33
James Hill <395+jameshill@users.noreply.github.com.>
2024-11-28 22:54:51 +1100

    Fix tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe Cart, type: :model do
+describe "Cart", type: :model do
   it "is a placeholder test" do
     base_sleep_duration = rand(1..3)
     final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
