7d81737d1ed084cbf2e71370422d45bfe0827850
James Hill <395+jameshill@users.noreply.github.com.>
2024-11-28 21:54:08 +1100

    Add some annotations and tags

--- spec/acceptance/flaky_spec.rb
+++ spec/acceptance/flaky_spec.rb
@@ -2,7 +2,31 @@ require "spec_helper"
 
 describe "Flaky Spec" do
   it "should pass most times but fail at the 11th hour" do
+    Buildkite::TestCollector.annotate('Testing Commenced')
+
     sleep(23)
+
+    Buildkite::TestCollector.annotate('Aggregation Commencing')
+
+    ActiveSupport::Notifications.instrument "sql.active_record", { sql: "SELECT complex_aggregation FROM thehumanfund" } do
+      sleep final_sleep_duration.round(3)
+    end
+
+    Buildkite::TestCollector.annotate('Aggregation Complete')
+    Buildkite::TestCollector.annotate('Snooze Round')
+
+    ActiveSupport::Notifications.instrument "sql.active_record", { sql: "SELECT faster_query FROM thehumanfund" } do
+      sleep(1)
+    end
+
+    Buildkite::TestCollector.annotate('Snoozing Complete')
+
+    ActiveSupport::Notifications.instrument "sql.active_record", { sql: "SELECT fastest_query FROM thehumanfund" } do
+      sleep(0.5)
+    end
+
+    Buildkite::TestCollector.annotate('Trace Completed')
+
     expect(Time.now.hour % 13).to_not eq(0)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,9 @@ require "buildkite/test_collector"
 Buildkite::TestCollector.configure(
   hook: :rspec,
   # url: "http://analytics-api.buildkite.localhost/v1/uploads",
-  token: ENV["BUILDKITE_ANALYTICS_TOKEN"]
+  token: ENV["BUILDKITE_ANALYTICS_TOKEN"],
+  env: {
+    build_id: ENV["BUILDKITE_BUILD_ID"],
+    step_id: ENV["BUILDKITE_STEP_ID"],
+  }
 )
