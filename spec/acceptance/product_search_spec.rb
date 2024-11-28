faa36de9f20a83a1757c87658a70d5f8c80c4bc7
James Hill <395+jameshill@users.noreply.github.com.>
2024-11-28 22:50:17 +1100

    Bunch of tests

---
+++ spec/acceptance/product_search_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe "Product Search flow", type: :feature do
+  it "is a placeholder acceptance test" do
+    base_sleep_duration = rand(10..20)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+end
---
+++ spec/features/admin_dashboard_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe "Admin Dashboard", type: :feature do
+  it "is a placeholder test" do
+    base_sleep_duration = rand(3..30)
+    final_sleep_duration = base_sleep_duration * rand(90..110)/100.0 * DURATION_MULTIPLIER
+    sleep final_sleep_duration.round(3)
+    expect(true).to eq(true)
+  end
+end
