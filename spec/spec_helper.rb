a7d3013916922a885380deb3ceaf7f33eda83639
James Hill <395+jameshill@users.noreply.github.com.>
2024-11-28 20:14:48 +1100

    Remove skipped tests step

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,23 +7,3 @@ Buildkite::TestCollector.configure(
   # url: "http://analytics-api.buildkite.localhost/v1/uploads",
   token: ENV["BUILDKITE_ANALYTICS_TOKEN"]
 )
-
-
-begin
-  skip_data = File.read('skipped.json')
-  skip = JSON.parse(skip_data)
-rescue
-  skip = []
-end
-
-skip_location = skip.reduce({}) { |m, t| m[t["location"]] = true; m }
-
-puts "disabled tests"
-puts skip_location.keys
-
-RSpec.configure do |c|
-  c.around :example do |example|
-    next if skip_location[example.location]
-    example.run
-  end
-end
