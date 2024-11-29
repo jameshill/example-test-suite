7f377f029924a9866bb5160f4c8a74b6b8e9c178
James Hill <395+jameshill@users.noreply.github.com.>
2024-11-29 15:00:37 +1100

    load metadata into spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,32 @@ require 'json'
 require "buildkite/test_collector"
 require "active_support"
 
+# Load the JSON from the environment variable
+metadata_json = ENV['INSTANCE_METADATA_JSON']
+
+# Check if the variable exists and parse it
+if metadata_json.nil? || metadata_json.empty?
+  puts "Environment variable INSTANCE_METADATA_JSON is not set or empty."
+end
+
+begin
+  # Parse the JSON string into a Ruby hash
+  metadata = JSON.parse(metadata_json)
+
+  # Access metadata as a hash
+  puts "Parsed Metadata:"
+  puts metadata
+
+  # Example: Access specific keys
+  puts "Instance Type: #{metadata['instance-type']}"
+  puts "Instance ID: #{metadata['instance-id']}"
+  puts "Instance Lifecycle: #{metadata['instance-life-cycle']}"
+  puts "Instance AZ: #{metadata['availability-zone']}"
+rescue JSON::ParserError => e
+  puts "Failed to parse JSON: #{e.message}"
+end
+
+
 Buildkite::TestCollector.configure(
   hook: :rspec,
   # url: "http://analytics-api.buildkite.localhost/v1/uploads",
